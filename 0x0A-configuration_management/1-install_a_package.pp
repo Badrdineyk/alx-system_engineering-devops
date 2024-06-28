86f45df1d5799616dcc8dfd71b5d5001117bad75
Badrdineyk <badrdineyk9@gmail.com>
2024-06-28 18:03:06 +0000

    Update the file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install flask from pip3.
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'Werkzeug':
+    ensure   => '2.2.2',
+    provider => 'pip3'
+}
+
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
